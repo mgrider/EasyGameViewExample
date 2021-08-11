b4bbf4e3a300b041bfbe4990a5ea90bbc722fb18
Martin Grider <martin@abstractpuzzle.com>
2021-08-11 02:49:04 -0500

    Initial Commit

---
+++ Shared/EasyGameViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyGameViewExampleApp.swift
+//  Shared
+//
+//  Created by Martin Grider on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct EasyGameViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
